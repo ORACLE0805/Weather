6398b6e89999230b05a848a73e17ffa58ff9ace6
997510352 <997510352@qq.com>
2019-12-15 23:46:51 +0800

    asdttt

--- app/src/main/java/com/example/weather/ChooseAreaFragment.java
+++ app/src/main/java/com/example/weather/ChooseAreaFragment.java
@@ -213,6 +213,7 @@ public class ChooseAreaFragment extends Fragment {
                     @Override
                     public void run() {
                         closeProgressDialog();
+
                         Toast.makeText(getContext(),"加载失败",Toast.LENGTH_SHORT).show();
                     }
                 });
